35c63b23b864977de6a7fed4b1ca25faa1d68660
r-ronnie-j <ronnie.jhaa@gmail.com>
2025-06-11 23:30:34 +0545

    request body in response shown

--- src/main/kotlin/com/ronnie/toastjet/swing/components/apiPanels/responsePanel/responseData/ResponseRequestPanel.kt
+++ src/main/kotlin/com/ronnie/toastjet/swing/components/apiPanels/responsePanel/responseData/ResponseRequestPanel.kt
@@ -1,42 +1,122 @@
 package com.ronnie.toastjet.swing.components.apiPanels.responsePanel.responseData
 
+import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.ui.JBColor
+import com.intellij.ui.components.JBLabel
+import com.intellij.util.ui.JBFont
+import com.intellij.util.ui.JBUI
+import com.ronnie.toastjet.model.enums.HttpMethod
+import com.ronnie.toastjet.swing.components.apiPanels.responsePanel.responseData.responseRequestBody.ConstructResReqBody
+import com.ronnie.toastjet.swing.components.apiPanels.responsePanel.responseData.responseRequestBody.ResponseRequestBodyPanel
 import com.ronnie.toastjet.swing.store.RequestStore
-import com.ronnie.toastjet.swing.widgets.CellParameter
-import com.ronnie.toastjet.swing.widgets.CustomTableWidget
-import java.awt.Dimension
-import javax.swing.BorderFactory
+import com.ronnie.toastjet.swing.widgets.RadioTabbedPanel
+import java.awt.BorderLayout
+import java.awt.FlowLayout
+import javax.swing.BoxLayout
+import javax.swing.JComponent
 import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.SwingConstants
 
-class ResponseRequestPanel (val store: RequestStore) : CustomTableWidget(
-    cellParameter = listOf(
-        CellParameter("Test", 10, 1.0),
-        CellParameter("Result", 10, 0.5),
-    )
-) {
-    override fun constructTableRow() {
-        val cookies = store.response.getState().tests
-        cookies.forEach { test ->
-            addRow(
-                listOf(
-                    createStyledLabel(if (test.name.length > 40) test.name.substring(0, 57) + "..." else test.name),
-                    createStyledLabel(if (test.result) "Success" else "Failure").apply {
-                        foreground = if (test.result) JBColor.GREEN else JBColor.RED
-                    },
-                )
-            )
-        }
+class ResponseRequestPanel(val store: RequestStore) : JPanel() {
+
+    val theme = EditorColorsManager.getInstance().globalScheme
+
+    val methodPanel = JLabel().apply {
+        border = JBUI.Borders.empty(4, 10, 5, 5)
+        font = JBFont.h3()
     }
 
-    private fun createStyledLabel(text: String): JLabel {
-        return JLabel().apply {
-            this.text = if (text.length > 80) text.substring(0, 80) + "..." else text
-            border = BorderFactory.createEmptyBorder(0, 8, 0, 8)
-            maximumSize = Dimension(300, 400)
+    val httpUrlPanel = JLabel().apply {
+        border = JBUI.Borders.empty(4, 5, 5,0)
+        font = JBFont.h3()
+    }
+
+    var urlPanel: JPanel = JPanel().apply {
+        layout = FlowLayout(FlowLayout.LEFT)
+        background = theme.defaultBackground
+        foreground = theme.defaultForeground
+        add(methodPanel)
+        add(httpUrlPanel)
+    }
+    var radioTabbedPanel = RadioTabbedPanel(
+        tabs = mutableListOf()
+    ).apply {
+        background = theme.defaultBackground
+        foreground = theme.defaultForeground
+    }
+
+    private fun addEmptyMessage(tabName: String): JComponent {
+        val panel = JPanel(BorderLayout()).apply {
+            background = theme.defaultBackground
+            val label = JBLabel("No $tabName").apply {
+                font = JBFont.h2()
+                foreground = theme.defaultForeground
+                horizontalAlignment = SwingConstants.CENTER
+            }
+            add(label, BorderLayout.CENTER)
         }
+        return panel
     }
 
     init {
-        constructTableRow()
+        layout = BoxLayout(this, BoxLayout.Y_AXIS)
+        background = theme.defaultBackground
+        foreground = theme.defaultForeground
+        populatePanel()
+        add(urlPanel)
+        add(radioTabbedPanel)
     }
+
+    fun populatePanel() {
+        val sendRequest = store.response.getState().apiRequestData
+
+        methodPanel.text = sendRequest.method.name.uppercase() + " : "
+        methodPanel.foreground = when (sendRequest.method) {
+            HttpMethod.GET -> JBColor.GREEN
+            HttpMethod.POST -> JBColor.YELLOW
+            HttpMethod.PUT -> JBColor.ORANGE
+            HttpMethod.PATCH -> JBColor.BLUE
+            HttpMethod.DELETE -> JBColor.RED
+            HttpMethod.HEAD -> JBColor.CYAN
+            else -> theme.defaultForeground
+        }
+        httpUrlPanel.text = sendRequest.url
+        val headersData = HashMap<String, String>()
+        sendRequest.headers.forEach {
+            if (it.isChecked) {
+                headersData[it.key] = it.value
+            }
+        }
+        if (headersData.isEmpty()) {
+            radioTabbedPanel.addTab("Headers", addEmptyMessage("Headers"))
+
+        } else {
+            radioTabbedPanel.addTab("Headers", ConstructResReqBody(headersData))
+        }
+
+        val paramsData = HashMap<String, String>()
+        sendRequest.params.forEach {
+            if (it.isChecked) {
+                headersData[it.key] = it.value
+            }
+        }
+        if (paramsData.isEmpty()) {
+            radioTabbedPanel.addTab("Params", addEmptyMessage("Params"))
+        } else {
+            radioTabbedPanel.addTab("Params", ConstructResReqBody(headersData))
+        }
+
+        val pathData = HashMap<String, String>()
+        sendRequest.path.forEach {
+            headersData[it.key] = it.value
+        }
+        if (pathData.isEmpty()) {
+            radioTabbedPanel.addTab("Paths", addEmptyMessage("Paths"))
+        } else {
+            radioTabbedPanel.addTab("Headers", ConstructResReqBody(headersData))
+        }
+        radioTabbedPanel.addTab("Body", ResponseRequestBodyPanel(store))
+    }
+
 }
