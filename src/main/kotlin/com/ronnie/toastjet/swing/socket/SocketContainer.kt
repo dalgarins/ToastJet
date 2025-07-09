49973b992996177f1e9faca655e017bed2fcc35e
r-ronnie-j <ronnie.jhaa@gmail.com>
2025-07-09 22:23:11 +0545

    layout changed now save socket response and work further

--- src/main/kotlin/com/ronnie/toastjet/swing/socket/SocketContainer.kt
+++ src/main/kotlin/com/ronnie/toastjet/swing/socket/SocketContainer.kt
@@ -12,8 +12,6 @@ import com.ronnie.toastjet.swing.store.ConfigStore
 import com.ronnie.toastjet.swing.store.SocketStore
 import java.awt.BorderLayout
 import java.awt.Dimension
-import java.awt.event.ComponentAdapter
-import java.awt.event.ComponentEvent
 import javax.swing.JPanel
 
 class SocketContainer(val store: SocketStore, val configStore: ConfigStore) : JPanel() {
@@ -29,44 +27,19 @@ class SocketContainer(val store: SocketStore, val configStore: ConfigStore) : JP
     }
 
     private val bottomScrollPane = JBScrollPane(SocketResPanel(store, configStore))
-    private val splitter = OnePixelSplitter(false, 0.5f).apply {
+    private val splitter = OnePixelSplitter(true, 0.5f).apply {
         dividerWidth = 1
         divider.background = JBColor.GRAY
+        firstComponent = topScrollPane
+        secondComponent = bottomScrollPane
     }
 
     init {
         layout = BorderLayout()
         border = JBUI.Borders.empty()
-
-        updateSplitterOrientation()
         add(splitter, BorderLayout.CENTER)
-
-        addComponentListener(object : ComponentAdapter() {
-            override fun componentResized(e: ComponentEvent?) {
-                updateSplitterOrientation()
-            }
-        })
         setTheme(store.theme.getState())
         store.theme.addListener(this::setTheme)
-    }
-
-    private fun updateSplitterOrientation() {
-        val isHorizontal = width <= 1500
-
-        splitter.apply {
-            if (orientation != isHorizontal) {
-                orientation = isHorizontal
-                firstComponent = topScrollPane
-                secondComponent = bottomScrollPane
-                proportion = 0.5f
-
-                border = JBUI.Borders.compound(
-                    if (isHorizontal) JBUI.Borders.emptyTop(25) else JBUI.Borders.empty(),
-                    JBUI.Borders.emptyLeft(20),
-                    JBUI.Borders.emptyRight(20)
-                )
-            }
-        }
     }
 
 }
